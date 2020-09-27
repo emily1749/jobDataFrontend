ebd9cd7e1cee7287fc3f2336ccdab6862fc7f926
Emily <linemily97@gmail.com>
2020-09-26 21:38:02 -0700

    bar charts

---
+++ src/BarChart.js
@@ -0,0 +1,12 @@
+// import React from "react";
+// import "./BarChart.css";
+
+// const BarChart = props => {
+//   // const
+//   return (
+//     // code
+//     <div>test</div>
+//   );
+// };
+
+// export default BarChart;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import axios from "axios";
+// import axios from "axios";
+import "./index.css";
+// import BarChart from "./BarChart";
+
+// function createBars(props) {
+//   console.log("here");
+
+//   //   code
+// }
 
 class App extends React.Component {
   constructor() {
@@ -8,74 +16,80 @@ class App extends React.Component {
     this.state = {
       city: "",
       state: "",
-      resultArray: [],
+      resultArray: [
+        ["typescript", "0.20", 0],
+        ["ruby", "1.52", 1],
+        ["python", "27.21", 2],
+        ["C++", "22.84", 3],
+        ["swift", "2.34", 4],
+        ["javascript", "17.16", 5],
+        ["php", "2.44", 6],
+        ["java", "17.56", 7],
+        ["C#", "8.22", 8],
+      ],
     };
   }
+
   onChange = e => {
     this.setState({
       [e.target.name]: e.target.value,
     });
   };
+  // barChart = () => {
+  //   //   test
+  //   console.log("here");
+  //   const test = this.resultArray[0];
+  //   return <div></div>;
+  // };
   onSubmit = e => {
-    e.preventDefault();
-    let resultArrayFetch = [];
-    let { city, state } = this.state;
-    city = city.replace(" ", "+");
-    console.log("city: " + city + "State:" + state);
-    var self = this;
-    let totalValue = 0;
-    axios
-      .get("https://jobskillsapi.emlin.repl.co/jobData/" + city + "/" + state)
-      .then(function(response) {
-        response = response.data;
-        //
-        // console.log(response);
-        // console.log("in axios");
-        // console.log(Object.keys(response)[1]);
-        // console.log(Object.values(response)[1]);
-        // for (i=0; i<Object.keys(response).length; i++){
-        // let total = 0;
-        Object.values(response).forEach(function(value) {
-          console.log("value:  " + value);
-          totalValue += value;
-          console.log("totalvalue:" + totalValue);
-        });
-        for (const [key, value] of Object.entries(response)) {
-          console.log(totalValue);
-          let keyResult = key;
-          let percentage = ((value / totalValue) * 100).toFixed(2);
-          if (keyResult === "C%23") {
-            keyResult = "C#";
-          }
-          if (keyResult === "C%2B%2B") {
-            keyResult = "C++";
-          }
-          //   if (percentage < 1) {
-          //     percentage = 1;
-          //   }
-          //   console.log("Key: " + key + " value: " + value);
-
-          resultArrayFetch.push([keyResult, percentage]);
-        }
-        console.log(resultArrayFetch);
-        self.setState({ resultArray: resultArrayFetch });
-        console.log("resultarray: " + self.state.resultArray);
-        // return resultArrayFetch;
-
-        // ["golang", 5]
-        // 1: (2) ["swift", 23]
-        // 2: (2) ["C%23", 83]
-        // 3: (2) ["python", 262]
-        // 4: (2) ["javascript", 163]
-        // 5: (2) ["typescript", 2]
-        // 6: (2) ["ruby", 13]
-        // 7: (2) ["php", 23]
-        // 8: (2) ["C%2B%2B", 221]
-        // 9: (2) ["java", 171]
-      });
-
-    // console.log("setstate" + this.state.resultArray);
+    //****BELOW- AXIOS WORKS */
+    // e.preventDefault();
+    // let resultArrayFetch = [];
+    // let { city, state } = this.state;
+    // city = city.replace(" ", "+");
+    // console.log("city: " + city + "State:" + state);
+    // var self = this;
+    // let totalValue = 0;
+    // let count = 0;
+    // axios
+    //   .get("https://jobskillsapi.emlin.repl.co/jobData/" + city + "/" + state)
+    //   .then(function(response) {
+    //     response = response.data;
+    //     Object.values(response).forEach(function(value) {
+    //       console.log("value:  " + value);
+    //       totalValue += value;
+    //       console.log("totalvalue:" + totalValue);
+    //     });
+    //     for (const [key, value] of Object.entries(response)) {
+    //       console.log(totalValue);
+    //       let keyResult = key;
+    //       let percentage = ((value / totalValue) * 100).toFixed(2);
+    //       if (keyResult === "C%23") {
+    //         keyResult = "C#";
+    //       }
+    //       if (keyResult === "C%2B%2B") {
+    //         keyResult = "C++";
+    //       }
+    //       resultArrayFetch.push([keyResult, percentage, count]);
+    //       count++; //for the key later on
+    //     }
+    //     console.log(resultArrayFetch);
+    //     self.setState({ resultArray: resultArrayFetch });
+    //     //0: (3) ["typescript", "0.20", 0]
+    //     // 1: (3) ["ruby", "1.52", 1]
+    //     // 2: (3) ["python", "27.21", 2]
+    //     // 3: (3) ["C++", "22.84", 3]
+    //     // 4: (3) ["swift", "2.34", 4]
+    //     // 5: (3) ["javascript", "17.16", 5]
+    //     // 6: (3) ["php", "2.44", 6]
+    //     // 7: (3) ["java", "17.56", 7]
+    //     // 8: (3) ["C#", "8.22", 8]
+    //     console.log("resultarray: " + self.state.resultArray);
+    //     console.log("resultarray: " + self.state.resultArray[1]);
+    // });
+    ///***AXIOS , UNCOMMENT LATER */
   };
+
   // componentDidMount() {
   //     //fetch here
   //   }
@@ -105,19 +119,45 @@ class App extends React.Component {
   render() {
     const { city, state } = this.state;
     return (
-      <form onSubmit={this.onSubmit}>
-        <label>City:</label>
-        <input type="text" name="city" value={city} onChange={this.onChange} />
+      <div>
+        <div>
+          <form onSubmit={this.onSubmit}>
+            <label>City:</label>
+            <input
+              type="text"
+              name="city"
+              value={city}
+              onChange={this.onChange}
+            />
 
-        <label>State:</label>
-        <input
-          type="text"
-          name="state"
-          value={state}
-          onChange={this.onChange}
-        />
-        <button> Submit </button>
-      </form>
+            <label>State:</label>
+            <input
+              type="text"
+              name="state"
+              value={state}
+              onChange={this.onChange}
+            />
+            <button> Submit </button>
+          </form>
+        </div>
+        <div className="barGraph">
+          {this.state.resultArray.map((value, index) => (
+            // test
+            <div className="bargroup" key={value[2]}>
+              <div
+                className="bar"
+                style={{ height: `${value[1] * 10}px` }}
+              ></div>
+              <h2>{value[0]}</h2>
+              <p>{value[1]}%</p>{" "}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      // <BarChart />
+
+      //   <BarChart />
     );
   }
   //   return <div>Hi there!</div>;
